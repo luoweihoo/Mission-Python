b681e482c1eb6f55938bf47e7ad90cac4d371962
Wei Luo <luoweihoo@yahoo.com>
2020-03-28 16:28:37 +0800

    Adding objects in room

---
+++ listing2-1.py
@@ -0,0 +1,12 @@
+# @File    :   listing2-1.py
+# @Time    :   2020/03/06 21:18:00
+# @Author  :   Wei Luo
+# @Version :   1.0
+# @Contact :   luoweihoo@yahoo.com
+# @Desc    :   None
+
+room_map = [[1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 3, 0, 0, 0],
+            [0, 0, 0, 0, 4]]
+print (room_map)
